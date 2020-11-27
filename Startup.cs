f69ae83025b9c19609774fb1c7d87d1ed081ed8b
Tony Yao <tony24862486@gmail.com>
2020-11-27 22:04:26 +0800

    Update db config name

--- Startup.cs
+++ Startup.cs
@@ -18,17 +18,19 @@ namespace VirturlMeetingAssitant.Backend
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MeetingContext>(options => options.UseNpgsql(Configuration["MeetingDatabase:Connection"]));
+            services.AddDbContext<MeetingContext>(options => options.UseNpgsql(Configuration["MeetingDatabase_Connection"]));
 
             #region Repositories
             services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
